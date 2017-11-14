7b2a340eed453d45c3b7c7a0e3d0523477a28dc1
elofmeister <eloy.schoemann@web.de>
2017-11-14 06:31:59 +0100

    Add bytedef

--- matrix.ino
+++ matrix.ino
@@ -1,3 +1,9 @@
+/*
+   LED - Matrix pin configuration
+*/
+
+//#define DEBUG_MODE 1
+
 #define ROW_1 10
 #define ROW_2 7
 #define ROW_3 12
@@ -17,92 +23,258 @@
 #define COL_8 A3
 
 const byte rows[] = {
-    ROW_1, ROW_2, ROW_3, ROW_4, ROW_5, ROW_6, ROW_7, ROW_8
+  ROW_1, ROW_2, ROW_3, ROW_4, ROW_5, ROW_6, ROW_7, ROW_8
 };
 
-// The display buffer
+const byte columns[] = {
+  COL_1, COL_2, COL_3, COL_4, COL_5, COL_6, COL_7, COL_8
+};
 
-byte linksoben[] = {B10000000, B00000000, B00000000, B00000000, B00000000, B00000000, B00000000, B00000000};
-byte rechtsoben[] = {B00000001, B00000000, B00000000, B00000000, B00000000, B00000000, B00000000, B00000000};
-byte linksunten[] = {B00000000, B00000000, B00000000, B00000000, B00000000, B00000000, B00000000, B10000000};
-byte rechtsunten[] = {B00000000, B00000000, B00000000, B00000000, B00000000, B00000000, B00000000, B00000001};
-byte alles[] = {B11111111,B11111111,B11111111,B11111111,B11111111,B11111111,B11111111,B11111111};
+#ifdef DEBUG_MODE
+const byte bytedef_topleft[] = {
+  B10000000,
+  B00000000,
+  B00000000,
+  B00000000,
+  B00000000,
+  B00000000,
+  B00000000,
+  B00000000
+};
+const byte bytedef_topright[] = {
+  B00000001,
+  B00000000,
+  B00000000,
+  B00000000,
+  B00000000,
+  B00000000,
+  B00000000,
+  B00000000
+};
+const byte bytedef_bottomleft[] = {
+  B00000000,
+  B00000000,
+  B00000000,
+  B00000000,
+  B00000000,
+  B00000000,
+  B00000000,
+  B10000000
+};
+const byte bytedef_bottomright[] = {
+  B00000000,
+  B00000000,
+  B00000000,
+  B00000000,
+  B00000000,
+  B00000000,
+  B00000000,
+  B00000001
+};
+const byte bytedef_all[] = {
+  B11111111,
+  B11111111,
+  B11111111,
+  B11111111,
+  B11111111,
+  B11111111,
+  B11111111,
+  B11111111
+};
+#endif
 
+const byte numbers[][8] = {
+  {
+    B00111000,
+    B01000100,
+    B01000100,
+    B01000100,
+    B01000100,
+    B01000100,
+    B01000100,
+    B00111000
+  },
+  {
+    B00010000,
+    B00110000,
+    B00010000,
+    B00010000,
+    B00010000,
+    B00010000,
+    B00010000,
+    B00111000
+  }, {
+    B00111000,
+    B01000100,
+    B00000100,
+    B00000100,
+    B00001000,
+    B00010000,
+    B00100000,
+    B01111100
+  }, {
+    B00111000,
+    B01000100,
+    B00000100,
+    B00011000,
+    B00000100,
+    B00000100,
+    B01000100,
+    B00111000
+  }, {
+    B00000100,
+    B00001100,
+    B00010100,
+    B00100100,
+    B01000100,
+    B01111100,
+    B00000100,
+    B00000100
+  }, {
+    B01111100,
+    B01000000,
+    B01000000,
+    B01111000,
+    B00000100,
+    B00000100,
+    B01000100,
+    B00111000
+  }, {
+    B00111000,
+    B01000100,
+    B01000000,
+    B01111000,
+    B01000100,
+    B01000100,
+    B01000100,
+    B00111000
+  }, {
+    B01111100,
+    B00000100,
+    B00000100,
+    B00001000,
+    B00010000,
+    B00100000,
+    B00100000,
+    B00100000
+  }, {
+    B00111000,
+    B01000100,
+    B01000100,
+    B00111000,
+    B01000100,
+    B01000100,
+    B01000100,
+    B00111000
+  }, {
+    B00111000,
+    B01000100,
+    B01000100,
+    B01000100,
+    B00111100,
+    B00000100,
+    B01000100,
+    B00111000
+  }
+};
 
-float timeCount = 0;
+const byte bytedef_clear[] = {
+  B00000000,
+  B00000000,
+  B00000000,
+  B00000000,
+  B00000000,
+  B00000000,
+  B00000000,
+  B00000000
+};
+
+uint16_t timerCnt = 0;
 
 void setup() {
-    // Open serial port
-    Serial.begin(9600);
-    
-    // Set all used pins to OUTPUT
-    // This is very important! If the pins are set to input
-    // the display will be very dim.
-    for (byte i = 2; i <= 13; i++)
-    {
-    pinMode(i, OUTPUT);
-    }
-    pinMode(A0, OUTPUT);
-    pinMode(A1, OUTPUT);
-    pinMode(A2, OUTPUT);
-    pinMode(A3, OUTPUT);
+  // Open serial port
+  Serial.begin(9600);
+
+  // Set all used pins to OUTPUT
+  // This is very important! If the pins are set to input
+  // the display will be very dim.
+  for (uint8_t i = 0; i < 8; i++)
+  {
+    pinMode(rows[i], OUTPUT);
+    pinMode(columns[i], OUTPUT);
+  }
 }
 
 void loop() {
-// This could be rewritten to not use a delay, which would make it appear brighter
-delay(5);
-timeCount += 1;
-if(timeCount <  70) {
-drawScreen(linksoben);
-} 
-else if (timeCount <  150) 
-{
-drawScreen(rechtsoben);
-}
-else if (timeCount <  270) 
-{
-drawScreen(linksunten);
-} 
-else if (timeCount <  350) 
-{
-drawScreen(rechtsunten);
-}
-else if (timeCount <  420) 
-{
-drawScreen(alles);
-}
-else if (timeCount <  720) 
-{
-  timeCount = 0;
-}
+#ifdef DEBUG_MODE
+  // This could be rewritten to not use a delay, which would make it appear brighter
+  delay(5);
+  timerCnt++;
+  if (timerCnt <  70) {
+    drawScreen(bytedef_topleft);
+  }
+  else if (timerCnt <  150)
+  {
+    drawScreen(bytedef_topright);
+  }
+  else if (timerCnt <  270)
+  {
+    drawScreen(bytedef_bottomleft);
+  }
+  else if (timerCnt <  350)
+  {
+    drawScreen(bytedef_bottomright);
+  }
+  else if (timerCnt <  420)
+  {
+    drawScreen(bytedef_all);
+  }
+  else if (timerCnt <  720)
+  {
+    timerCnt = 0;
+  }
+#else
+  delay(5);
+  timerCnt++;
+  if (timerCnt / 100 + 1 > sizeof(numbers) / sizeof(numbers[0])) {    // 100 * 5ms = 500ms .. display time per number
+    timerCnt = 0;
+  }
+  if (timerCnt % 100) {
+    drawScreen(numbers[timerCnt / 100]);
+  }
+  else {
+    drawScreen(bytedef_clear);
+    delay(50);  // 50ms .. gaps between animations
+  }  
+#endif
 
 
 }
 
- void  drawScreen(byte buffer2[]){
-     
-    
-   // Turn on each row in series
-   for (byte i = 0; i < 8; i++) {
-        setColumns(buffer2[i]); // Set columns for this specific row
-        
-        digitalWrite(rows[i], HIGH);
-        delay(1); // Set this to 50 or 100 if you want to see the multiplexing effect!
-        digitalWrite(rows[i], LOW);
-        
-    }
+void  drawScreen(const byte buf[]) {
+
+
+  // Turn on each row in series
+  for (byte i = 0; i < 8; i++) {
+    setColumns(buf[i]); // Set columns for this specific row
+
+    digitalWrite(rows[i], HIGH);
+    delay(1); // Set this to 50 or 100 if you want to see the multiplexing effect!
+    digitalWrite(rows[i], LOW);
+
+  }
 }
 
 
-void setColumns(byte b) {
-    digitalWrite(COL_1, (~b >> 0) & 0x01); // Get the 1st bit: 10000000
-    digitalWrite(COL_2, (~b >> 1) & 0x01); // Get the 2nd bit: 01000000
-    digitalWrite(COL_3, (~b >> 2) & 0x01); // Get the 3rd bit: 00100000
-    digitalWrite(COL_4, (~b >> 3) & 0x01); // Get the 4th bit: 00010000
-    digitalWrite(COL_5, (~b >> 4) & 0x01); // Get the 5th bit: 00001000
-    digitalWrite(COL_6, (~b >> 5) & 0x01); // Get the 6th bit: 00000100
-    digitalWrite(COL_7, (~b >> 6) & 0x01); // Get the 7th bit: 00000010
-    digitalWrite(COL_8, (~b >> 7) & 0x01); // Get the 8th bit: 00000001
+void setColumns(const byte b) {
+  digitalWrite(COL_1, (~b >> 0) & 0x01); // Get the 1st bit: 10000000
+  digitalWrite(COL_2, (~b >> 1) & 0x01); // Get the 2nd bit: 01000000
+  digitalWrite(COL_3, (~b >> 2) & 0x01); // Get the 3rd bit: 00100000
+  digitalWrite(COL_4, (~b >> 3) & 0x01); // Get the 4th bit: 00010000
+  digitalWrite(COL_5, (~b >> 4) & 0x01); // Get the 5th bit: 00001000
+  digitalWrite(COL_6, (~b >> 5) & 0x01); // Get the 6th bit: 00000100
+  digitalWrite(COL_7, (~b >> 6) & 0x01); // Get the 7th bit: 00000010
+  digitalWrite(COL_8, (~b >> 7) & 0x01); // Get the 8th bit: 00000001
 
 }
 
